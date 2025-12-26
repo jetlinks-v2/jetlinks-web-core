921c045b058bb81a160fdd381e0c021c5f0aed3f
haodd0216 <1903289197@qq.com>
2025-12-26 11:02:58 +0800

    fix: 修改协议表格列内容渲染

--- public/protocol/MODBUS_TCP/pointColumns.vue
+++ public/protocol/MODBUS_TCP/pointColumns.vue
@@ -1,35 +1,41 @@
 <script setup>
-import {inject, ref} from "vue";
+import {inject, ref, h} from "vue";
+import { Ellipsis } from '@jetlinks-web/components'
 
 const pointColumns = inject("point-extra-columns", ref([]))
 pointColumns.value = [
   {
     title: '寄存器数量',
-    dataIndex: '11',
-    key: '11',
+    dataIndex: 'quantity',
+    key: 'quantity',
     ellipsis: true,
+    width: 150,
     scopedSlots: true,
-    width: 100
+    customRender: ({record}) => {
+      return h('span', {}, `${record.configuration?.parameter?.quantity || '--'}`)
+    }
   },
   {
     title: '缩放因子',
-    dataIndex: '22',
-    key: '22',
+    dataIndex: 'scaleFactor',
+    key: 'scaleFactor',
     scopedSlots: true,
-    width: 200,
+    width: 100,
+    customRender: ({record}) => {
+      return h('span', {}, `${record.configuration?.codec?.configuration?.scaleFactor || '--'}`)
+    }
   },
   {
     title: '小数保留位',
-    dataIndex: '33',
-    key: '33',
+    dataIndex: 'scale',
+    key: 'scale',
     scopedSlots: true,
     ellipsis: true,
-    width: 100
+    width: 150,
+    customRender: ({record}) => {
+      return h('span', {}, `${record.configuration?.codec?.configuration?.scale || '--'}`)
+    }
   },
 ]
 
 </script>
-
-<template>
-  <div>哈哈哈哈哈哈哈</div>
-</template>
