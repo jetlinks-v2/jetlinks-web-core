a2c394a1d04b3f894e142a5585a2ae011188c8c9
XieYongHong <18010623010@163.com>
2026-01-08 11:25:48 +0800

    feat: 添加数组同上工具

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,8 @@
 // 是否不是community版本
 import { getToken, randomString, setToken } from '@jetlinks-web/utils'
-import {BASE_API} from "@jetlinks-web/constants";
+import { BASE_API } from "@jetlinks-web/constants";
 import { PersonalAIKey, PersonalKey, PersonalToken } from '@jetlinks-web-core/utils/consts'
+import { get, set } from 'lodash-es'
 
 export const isNoCommunity = !(localStorage.getItem('version_code') === 'community');
 
@@ -38,4 +39,58 @@ export const initPersonal = () => {
     PersonalToken.value = _token
     PersonalToken.aiToken = url.searchParams.get(PersonalAIKey)
   }
+}
+
+// 获取上一行
+export function getEffectivePrevRow(rows: Record<string, any>, index: number) {
+  let i = index - 1;
+  while (i >= 0 && rows[i].sameAsAbove) {
+    i--;
+  }
+  return i >= 0 ? rows[i] : null;
+}
+
+/**
+ * 执行同上逻辑
+ * @param rows
+ * @param index
+ * @param checked
+ */
+export function applySameAsAbove(rows: Record<string, any>, index: number, checked: boolean) {
+  const current = rows[index];
+  current.sameAsAbove = checked;
+
+  if (!checked) {
+    current.disabled = false;
+    return rows;
+  }
+
+  const prev = getEffectivePrevRow(rows, index);
+  if (!prev) {
+    current.sameAsAbove = false;
+    return rows;
+  }
+
+  // 复制字段（只复制业务字段）
+  Object.assign(current, {
+    name: prev.name,
+    code: prev.code,
+    disabled: true
+  });
+
+  return rows;
+}
+
+function syncFollowingSameRows(rows: Record<string, any>, index: number) {
+  let base = rows[index];
+
+  for (let i = index + 1; i < rows.length; i++) {
+    if (!rows[i].sameAsAbove) break;
+
+    rows[i].name = base.name;
+    rows[i].code = base.code;
+    rows[i].disabled = true;
+  }
+
+  return rows;
 }
