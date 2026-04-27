4f0d6b5ae452c073123bc48e7d9c6c44daf84579
XieYongHong <18010623010@163.com>
2026-04-27 18:51:12 +0800

    fix: useTabSaveSuccess兼容获取domain失败的情况

--- src/hooks/useTabSaveSuccess.ts
+++ src/hooks/useTabSaveSuccess.ts
@@ -35,8 +35,10 @@ export const useTabSaveSuccess = (code: string = '', options?: OptionsType) => {
       }
 
       const _params = new URLSearchParams({...params, sourceId: id.value})
-      const beforeHash = isSaaS ? `/${localStorage.getItem('X-Tenant-Domain')}` : ''
-      const hash = location.hash ? `${beforeHash}/#` : ''
+      const domain = localStorage.getItem('X-Tenant-Domain') || ''
+      const domainPath = domain ? `/${domain}` : ''
+      const beforeHash = isSaaS ? `${domainPath}/#` : ''
+      const hash = location.hash ? `${beforeHash}` : ''
       const path = _options.menuParams ? formatPath(menuItem.path, _options.menuParams)  : menuItem.path
       const url = [location.origin, hash, path, '?', _params.toString()].join('')
 
